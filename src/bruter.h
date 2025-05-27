37e2ab032d483cddd2535fffb055bbaac632b340
jardimdanificado <jardeltorreslima01@gmail.com>
2025-05-27 12:53:19 -0300

    bruter now does use bruter_ prefix instead list_

--- src/bruter.h
+++ src/bruter.h
@@ -16,73 +16,73 @@
 // version
 #define VERSION "0.8.6"
 
-typedef intptr_t Int;
-typedef uintptr_t UInt;
+typedef intptr_t BruterInt;
+typedef uintptr_t BruterUInt;
 
-// BRUTER use Int and Float instead of int and float because we need to use always the pointer size for any type that might share the fundamental union type;
+// BRUTER use BruterInt and BruterFloat instead of int and float because we need to use always the pointer size for any type that might share the fundamental union type;
 // BRUTER use a union as universal type, and BRUTER is able to manipulate and use pointers direcly so we need to use the pointer size;
 #if INTPTR_MAX == INT64_MAX
-typedef double Float;
+typedef double BruterFloat;
 #else
-typedef float Float;
+typedef float BruterFloat;
 #endif
 
-// Value
+// BruterValue
 typedef union 
 {
     // these types depend on the size of the pointer
-    Float f;
-    Int i;
-    UInt u;
+    BruterFloat f;
+    BruterInt i;
+    BruterUInt u;
 
     // these types are pointers
     char* s;
     void* p;
     
     // these types are int arrays
-    uint8_t u8[sizeof(Float)];
-    uint16_t u16[sizeof(Float) / 2];
-    uint32_t u32[sizeof(Float) / 4];
+    uint8_t u8[sizeof(BruterFloat)];
+    uint16_t u16[sizeof(BruterFloat) / 2];
+    uint32_t u32[sizeof(BruterFloat) / 4];
 
-    int8_t i8[sizeof(Float)];
-    int16_t i16[sizeof(Float) / 2];
-    int32_t i32[sizeof(Float) / 4];
+    int8_t i8[sizeof(BruterFloat)];
+    int16_t i16[sizeof(BruterFloat) / 2];
+    int32_t i32[sizeof(BruterFloat) / 4];
 
-    float f32[sizeof(Float) / 4];
-} Value;
-#define VALUE(type, value) (Value){.type = value}
+    float f32[sizeof(BruterFloat) / 4];
+} BruterValue;
+#define BRUTER_VALUE(type, value) (BruterValue){.type = value}
 
 typedef struct 
 {
-    Value *data;
+    BruterValue *data;
     char **keys;
-    Int size;
-    Int capacity;
-} List;
+    BruterInt size;
+    BruterInt capacity;
+} BruterList;
 
-// List functions(also work for tables as tables are just lists with keys)
-List   *list_init(Int size, bool is_table);
-void    list_free(List *list);
-void    list_double(List *list);
-void    list_half(List *list);
-void    list_push(List *list, Value value, const char* key);
-void    list_unshift(List *list, Value value, const char* key);
-void    list_insert(List *list, Int i, Value value, const char* key);
-Value   list_pop(List *list);
-Value   list_shift(List *list);
-Value   list_remove(List *list, Int i);
-Value   list_fast_remove(List *list, Int i);
-void    list_swap(List *list, Int i1, Int i2);
-Int     list_find(List *list, Value value, const char* key);
-void    list_reverse(List *list);
+// BruterList functions(also work for tables as tables are just lists with keys)
+BruterList   *bruter_init(BruterInt size, bool is_table);
+void          bruter_free(BruterList *list);
+void          bruter_double(BruterList *list);
+void          bruter_half(BruterList *list);
+void          bruter_push(BruterList *list, BruterValue value, const char* key);
+void          bruter_unshift(BruterList *list, BruterValue value, const char* key);
+void          bruter_insert(BruterList *list, BruterInt i, BruterValue value, const char* key);
+BruterValue   bruter_pop(BruterList *list);
+BruterValue   bruter_shift(BruterList *list);
+BruterValue   bruter_remove(BruterList *list, BruterInt i);
+BruterValue   bruter_fast_remove(BruterList *list, BruterInt i);
+void          bruter_swap(BruterList *list, BruterInt i1, BruterInt i2);
+BruterInt     bruter_find(BruterList *list, BruterValue value, const char* key);
+void          bruter_reverse(BruterList *list);
 // if context is NULL, it will call direcly from list->data[0].p and the result itself
 // if context is not NULL, it will call from context->data[list->data[0].i].p and return the index of the result in context
 // if context is not NULL, the result will be always an int, because it return the index of the result in context
-Value   list_call(List *context, List *list);
+BruterValue   bruter_call(BruterList *context, BruterList *list);
 
 // experimental
-List   *list_copy(List *list);
+BruterList   *bruter_copy(BruterList *list);
 
-#define LIST_FUNCTION(name) Int name(List *context, List *args)
+#define BRUTER_FUNCTION(name) BruterInt name(BruterList *context, BruterList *args)
 
 #endif
