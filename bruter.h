84fc8b7fca8eb34683ab2c8c9ee4a80753b733ca
jardimdanificado <jardeltorreslima01@gmail.com>
2025-08-02 12:38:52 -0300

    new function bruter_parse

--- bruter.h
+++ bruter.h
@@ -12,6 +12,7 @@
 #include <string.h>
 #include <inttypes.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 // version
 #define BRUTER_VERSION "0.9.1"
@@ -73,6 +74,23 @@ struct BruterList
     int8_t *types;
     // the data, it is a pointer to a BruterValue array
     BruterValue *data;
+};
+
+typedef void (*Function)(BruterList *stack);
+
+#define function(name) \
+    void name(BruterList *stack)
+
+#define init(name) \
+    void init_##name(BruterList *context)
+
+enum {
+    BRUTER_TYPE_NULL = 0,
+    BRUTER_TYPE_ANY,
+    BRUTER_TYPE_FLOAT,
+    BRUTER_TYPE_BUFFER,
+    BRUTER_TYPE_LIST,
+    BRUTER_TYPE_FUNCTION,
 };
 
 STATIC_INLINE BruterValue        bruter_value_int(BruterInt value);
@@ -195,6 +213,8 @@ STATIC_INLINE void               bruter_set_type(const BruterList *list, BruterI
 STATIC_INLINE const char*        bruter_get_version(void);
 // arena   
 STATIC_INLINE void*              bruter_alloc(BruterList *arena, size_t size);
+// bruter representation
+STATIC_INLINE BruterList*        bruter_parse(BruterList *context, const char* input_str);
 
 // functions implementations
 // functions implementations
@@ -1522,5 +1542,94 @@ STATIC_INLINE void* bruter_alloc(BruterList* arena, size_t size)
     arena->size += aligned_size;
     return ptr;
 }
+
+STATIC_INLINE BruterList* bruter_parse(BruterList *context, const char* input_str)
+{
+    BruterList *stack = bruter_new(8, true, true);
+    BruterList *splited = bruter_new(8, false, false);
+    char* original_str = strdup(input_str); // Duplicate the input string to avoid modifying the original
+    char* token = strtok(original_str, "\n\t\r ");
+    while (token != NULL)
+    {
+        bruter_push_pointer(splited, token, NULL, 0);
+        token = strtok(NULL, "\n\t\r ");
+    }
+
+    for (BruterInt i = 0; i < splited->size; i++)
+    {
+        char* token = bruter_get_pointer(splited, i);
+        //if (token == NULL || token[0] == '\0') continue; // Skip empty tokens
+        
+        if (token[0] == '!') // call stack
+        {
+            Function func = bruter_pop_pointer(stack);
+            func(stack);
+        }
+        else if (token[0] == '&' && token[1] == '\0') // stack
+        {
+            bruter_push_pointer(stack, stack, NULL, BRUTER_TYPE_LIST);
+            continue;
+        }
+        else if (token[0] == '@' && token[1] == '\0') // context
+        {
+            bruter_push_pointer(stack, context, NULL, BRUTER_TYPE_LIST);
+            continue;
+        }
+        else if (token[0] == '?') // ifgo, the only control operator
+        {
+            BruterInt condition = bruter_pop_int(stack);
+            BruterInt iftrue_position = bruter_pop_int(stack);
+            if (condition)
+            {
+                i = iftrue_position - 1;
+            }
+        }
+        else if (isdigit(token[0]) || (token[0] == '-' && isdigit(token[1]))) // number
+        {
+            if (strchr(token, '.')) // float
+            {
+                bruter_push_float(stack, strtof(token, NULL), NULL, BRUTER_TYPE_FLOAT);
+            }
+            else // int
+            {
+                bruter_push_int(stack, strtol(token, NULL, 10), NULL, BRUTER_TYPE_ANY);
+            }
+        }
+        else if (token[0] == '#') // string
+        {
+            char* str = strdup(token + 1);
+            for (char* p = str; *p; p++) 
+            {
+                if (*p == '\x14') *p = '\n'; // Replace ASCII 20 with newline
+                else if (*p == '\x15') *p = '\r'; // Replace ASCII 21 with carriage return
+                else if (*p == '\x16') *p = '\t'; // Replace ASCII 22 with tab
+                else if (*p == '\x17') *p = ' '; // Replace ASCII 23 with space
+                else if (*p == '\x18') *p = ':'; // Replace ASCII 24 with colon
+            }
+            bruter_push_pointer(stack, str, NULL, BRUTER_TYPE_BUFFER);
+        }
+        else // find variable
+        {
+            BruterInt found = bruter_find_key(context, token);
+            if (found != -1)
+            {
+                BruterMeta meta = bruter_get_meta(context, found);
+                meta.key = NULL; // we don't need the key here
+                bruter_push_meta(stack, meta);
+            }
+            else
+            {
+                // If not found
+                printf("WARNING: Variable '%s' not found in context\n", token);
+                // We can still push a null value to the stack
+                bruter_push_int(stack, 0, NULL, BRUTER_TYPE_NULL);
+            }
+        }
+    }
+    bruter_free(splited); // Free the temporary list
+    free(original_str); // free the original string
+    return stack;
+}
+
 #endif // ifndef BRUTER_AS_HEADER macro
 #endif // ifndef BRUTER_H macro
