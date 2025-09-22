9acfb31d34552f92a05617f763297cf5d0909107
jardimdanificado <jardeltorreslima01@gmail.com>
2025-09-22 04:52:26 -0300

    code is % again ans static is # again

--- bruter.h
+++ bruter.h
@@ -15,7 +15,7 @@
 #include <ctype.h>
 
 // version
-#define BRUTER_VERSION "0.9.3"
+#define BRUTER_VERSION "0.9.2a"
 
 typedef intptr_t BruterInt;
 typedef uintptr_t BruterUInt;
@@ -1581,7 +1581,7 @@ static inline void bruter_interpret(BruterList *context, const char* input_str, 
             break;
             case '&': // stack
             case '@': // context
-            case '~': // code
+            case '%': // code
             {
                 BruterList* list = NULL;
                 if(token[0] == '&')
@@ -1592,7 +1592,7 @@ static inline void bruter_interpret(BruterList *context, const char* input_str, 
                 {
                     list = context;
                 }
-                else if (token[0] == '~')
+                else if (token[0] == '%')
                 {
                     list = code;
                 }
@@ -1699,7 +1699,7 @@ static inline void bruter_interpret(BruterList *context, const char* input_str, 
             default:
             {
                 BruterInt found = -1;
-                if (token[0] == '%') // static values are not re-evaluated when encountered again
+                if (token[0] == '#') // static values are not re-evaluated when encountered again
                 {
                     if (isdigit(token[1]))
                     {
